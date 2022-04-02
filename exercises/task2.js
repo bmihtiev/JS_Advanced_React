91a8cd2d19639317e4309f6c691b21fa584373a0
bmihtiev <bmihtiev@gmail.com>
2022-04-03 00:49:33 +0300

    Regular Expression homework

--- exercises/task2.js
+++ exercises/task2.js
@@ -2,7 +2,7 @@
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const sumOfSquaresOfEvenNumbers = numbers.filter(number => number % -2 === 0)
+const sumOfSquaresOfEvenNumbers = numbers.filter(number => number % 2 === 0)
     .reduce((acc, curr) => acc + curr ** 2, 0)
 
 console.log(sumOfSquaresOfEvenNumbers)
